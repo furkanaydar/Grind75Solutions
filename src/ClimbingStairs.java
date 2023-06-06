ae8268dd9a46601c7d51378c6d20225194f52887
FurkanAydar <furkanaydar97@gmail.com>
2023-06-06 13:44:02 +0300

    ClimbingStairs solution

---
+++ src/ClimbingStairs.java
@@ -0,0 +1,20 @@
+public class ClimbingStairs {
+    public int climbStairs(int n) {
+        int[] d = new int[46];
+        d[0] = 0;
+        d[1] = 1;
+        d[2] = 2;
+        /*
+        1 1 2
+        1 2 1
+        2 1 1
+        2 2
+        1 1 1 1
+         */
+        for (int i=3; i<=n; i++) {
+            d[i] = d[i-1] + d[i-2];
+        }
+
+        return d[n];
+    }
+}
