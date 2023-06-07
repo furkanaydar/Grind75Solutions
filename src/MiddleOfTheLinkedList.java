6aff0ab83402b04c90e4a3a71d311f86e9eb9c25
FurkanAydar <furkanaydar97@gmail.com>
2023-06-07 19:21:08 +0300

    MiddleOfTheLinkedList

---
+++ src/MiddleOfTheLinkedList.java
@@ -0,0 +1,48 @@
+public class MiddleOfTheLinkedList {
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    public static ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+
+    public static ListNode case1() {
+        return new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
+    }
+
+
+    public static ListNode case2() {
+        return new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5, new ListNode(6))))));
+    }
+
+    public static ListNode case3() {
+        return new ListNode(1);
+    }
+
+    public static ListNode case4() {
+        return new ListNode(1, new ListNode(2));
+    }
+
+    public static void main(String[] args) {
+        System.out.println(middleNode(case1()).val);
+        System.out.println(middleNode(case2()).val);
+        System.out.println(middleNode(case3()).val);
+        System.out.println(middleNode(case4()).val);
+
+    }
+
+}
