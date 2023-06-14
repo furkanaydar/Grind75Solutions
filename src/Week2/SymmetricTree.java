b16a347fb20fba2a3a32c961aaeb3790dbcb76aa
FurkanAydar <furkanaydar97@gmail.com>
2023-06-14 21:54:28 +0300

    MissingNumber

--- src/Week2/SymmetricTree.java
+++ src/Week2/SymmetricTree.java
@@ -1,24 +1,8 @@
 package Week2;
 
+import common.TreeNode;
+
 public class SymmetricTree {
-    public static class TreeNode {
-        int val;
-        TreeNode left;
-        TreeNode right;
-
-        TreeNode() {
-        }
-
-        TreeNode(int val) {
-            this.val = val;
-        }
-
-        TreeNode(int val, TreeNode left, TreeNode right) {
-            this.val = val;
-            this.left = left;
-            this.right = right;
-        }
-    }
 
     public boolean c(TreeNode l, TreeNode r) {
         if (l == null || r == null) {
